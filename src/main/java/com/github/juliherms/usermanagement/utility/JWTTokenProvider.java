5971b513759e00d24bd4f606cffa0ab2dcc5b9be
Juliherms Vasconcelos <j.a.vasconcelos321@gmail.com>
2023-04-26 23:07:51 -0300

    feat: add get more information from token

--- src/main/java/com/github/juliherms/usermanagement/utility/JWTTokenProvider.java
+++ src/main/java/com/github/juliherms/usermanagement/utility/JWTTokenProvider.java
@@ -6,10 +6,16 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.github.juliherms.usermanagement.constant.SecurityConstant;
 import com.github.juliherms.usermanagement.domain.UserPrincipal;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -20,6 +26,7 @@ import static java.util.Arrays.stream;
 /**
  * this class responsible to generate token
  */
+@Component
 public class JWTTokenProvider {
 
     @Value("${jwt.secret}")
@@ -40,6 +47,29 @@ public class JWTTokenProvider {
     public List<GrantedAuthority> getAuthorities(String token) {
         String[] claims = getClaimsFromToken(token);
         return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    public Authentication getAuthentication(String username, List<GrantedAuthority> authorities, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken userPasswordAuthToken =
+                new UsernamePasswordAuthenticationToken(username,null,authorities);
+
+        userPasswordAuthToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        return userPasswordAuthToken;
+    }
+
+    public boolean isTokenValid(String username, String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return StringUtils.isNotEmpty(username) && !isTokenExpired(verifier,token);
+    }
+
+    public String getSubject(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getSubject();
+    }
+
+    private boolean isTokenExpired(JWTVerifier verifier, String token) {
+        Date expiration = verifier.verify(token).getExpiresAt();
+        return expiration.before(new Date());
     }
 
     private String[] getClaimsFromToken(String token) {
