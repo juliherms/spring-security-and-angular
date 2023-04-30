3502acf0300ba0dfb2e0a09b271e68b56a806154
Juliherms Vasconcelos <j.a.vasconcelos321@gmail.com>
2023-04-29 23:23:53 -0300

    feat: add customizer access denied

--- src/main/java/com/github/juliherms/usermanagement/filter/JwtAccessDeniedHandler.java
+++ src/main/java/com/github/juliherms/usermanagement/filter/JwtAccessDeniedHandler.java
@@ -5,37 +5,35 @@ import com.github.juliherms.usermanagement.constant.SecurityConstant;
 import com.github.juliherms.usermanagement.domain.HttpResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 
 /**
- * This class responsible to customize forbidden response
+ * This class responsible to customize access denied
  */
 @Component
-public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
-
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
-
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
         HttpResponse httpResponse = new HttpResponse(
-                HttpStatus.FORBIDDEN.value(),
-                HttpStatus.FORBIDDEN,
-                HttpStatus.FORBIDDEN.getReasonPhrase().toUpperCase(),
-                SecurityConstant.FORBIDDEN_MESSAGE);
+                HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED,
+                HttpStatus.UNAUTHORIZED.getReasonPhrase().toUpperCase(),
+                SecurityConstant.ACCESS_DENIED_MESSAGE);
 
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
 
         OutputStream outputStream = response.getOutputStream();
         ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(outputStream, httpResponse);
         outputStream.flush();
     }
-
 }
