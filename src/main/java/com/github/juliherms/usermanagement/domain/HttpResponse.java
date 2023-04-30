68c7f442ca166641e8c8c4c93bfa9411f7c7da48
Juliherms Vasconcelos <j.a.vasconcelos321@gmail.com>
2023-04-30 11:33:09 -0300

    feat: add timestamp in the custom error object

--- src/main/java/com/github/juliherms/usermanagement/domain/HttpResponse.java
+++ src/main/java/com/github/juliherms/usermanagement/domain/HttpResponse.java
@@ -1,9 +1,12 @@
 package com.github.juliherms.usermanagement.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
+
+import java.util.Date;
 
 /**
  * This class responsible to represent http return from endpoint
@@ -12,9 +15,19 @@ import org.springframework.http.HttpStatus;
 @NoArgsConstructor
 @Data
 public class HttpResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "America/New_York")
+    private Date timestamp;
     private int httpStatusCode; // 200, 201, 400, 500
     private HttpStatus httpStatus;
     private String reason;
     private String message;
+
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timestamp = new Date();
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
 
 }
